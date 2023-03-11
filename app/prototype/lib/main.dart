5e48e2842ef30fee7ca6b92c54176d23b9b12145
marco-vb <marcofilipe03@hotmail.com>
2023-03-11 21:37:19 +0000

    main now calls widget tree

--- app/prototype/lib/main.dart
+++ app/prototype/lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:prototype/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:prototype/widget_tree.dart';
 
-void main(List<String> args) {
-  runApp(const MaterialApp(home: HomeScreen()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GameShare',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const WidgetTree(),
+    );
+  }
 }
