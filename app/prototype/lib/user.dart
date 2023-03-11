8a380cb46d265dce4f1318504fa98ffe1a5b9a77
marco-vb <marcofilipe03@hotmail.com>
2023-03-11 21:39:19 +0000

    user page showing email

--- app/prototype/lib/user.dart
+++ app/prototype/lib/user.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:prototype/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:prototype/search.dart';
 import 'package:prototype/home.dart';
 
@@ -10,81 +12,115 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+  final User? user = Auth().user;
   int _selected = 2;
+
+  Future<void> signOut() async {
+    await Auth().signOut();
+  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: const TextSpan(
+          text: 'GameShare',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+            color: Colors.green,
+          )),
+    );
+  }
+
+  Widget _userUid() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+          text: user?.email ?? 'No User',
+          style: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+            color: Colors.green,
+          )),
+    );
+  }
+
+  Widget _signOutButton() {
+    return InkWell(
+      onTap: () async {
+        await signOut();
+      },
+      child: Container(
+        width: 130,
+        padding: const EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            border: Border.all(color: Colors.green, width: 2)),
+        child: const Text('Sign out',
+            style: TextStyle(fontSize: 20, color: Colors.green)),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.pink,
-          title: const Text('GameShare'),
+          title: _title(),
         ),
-        body: Scaffold(
-          body: Scaffold(
-            backgroundColor: Colors.green,
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
-                  Text(
-                    'User Profile',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
-              ),
-            ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _userUid(),
+              _signOutButton(),
+            ],
           ),
-          bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-                  label: 'Search',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: 'User',
-                ),
-              ],
-              currentIndex: _selected,
-              selectedItemColor: Colors.green,
-              onTap: (int index) {
-                switch (index) {
-                  case 0:
-                    Navigator.push(
-                      context,
-                      PageRouteBuilder(
-                        pageBuilder: (_, __, ___) => const HomeScreen(),
-                        transitionDuration: const Duration(seconds: 1),
-                        transitionsBuilder: (_, a, __, c) =>
-                            FadeTransition(opacity: a, child: c),
-                      ),
-                    );
-                    break;
-                  case 1:
-                    Navigator.push(
-                      context,
-                      PageRouteBuilder(
-                        pageBuilder: (_, __, ___) => const SearchScreen(),
-                        transitionDuration: const Duration(seconds: 1),
-                        transitionsBuilder: (_, a, __, c) =>
-                            FadeTransition(opacity: a, child: c),
-                      ),
-                    );
-                    break;
-                  case 2:
-                    break;
-                }
-                setState(() {
-                  _selected = index;
-                });
-              }),
         ),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'User',
+            ),
+          ],
+          currentIndex: _selected,
+          selectedItemColor: Colors.pink,
+          onTap: (int index) {
+            if (index == 0) {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => const HomeScreen(),
+                  transitionDuration: const Duration(seconds: 1),
+                  transitionsBuilder: (_, a, __, c) =>
+                      FadeTransition(opacity: a, child: c),
+                ),
+              );
+            } else if (index == 1) {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => const SearchScreen(),
+                  transitionDuration: const Duration(seconds: 1),
+                  transitionsBuilder: (_, a, __, c) =>
+                      FadeTransition(opacity: a, child: c),
+                ),
+              );
+            }
+            setState(() {
+              _selected = index;
+            });
+          },
+        ));
   }
 }
