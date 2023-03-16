81b139835ab16b98b4ffc7314cb97a04b61f99a1
ZeAntonioM <joseapmartins2003@gmail.com>
2023-03-16 16:37:33 +0000

    setup files and directories

---
+++ app/gameshare/android/app/src/main/kotlin/com/example/gameshare/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gameshare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app/gameshare/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
