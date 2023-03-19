0f83d531d3358c41a9a6a7ac855c08e1c1360cf2
dsantosferreira <unidsferreira2003@gmail.com>
2023-03-19 19:50:53 +0000

    Removed widget used for testing

--- app/gameshare/lib/main.dart
+++ app/gameshare/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:gameshare/services/api_requests.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  fetchGames(page: 2);
   runApp(const MyApp());
 }
 
@@ -14,25 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
-      title: 'haahah',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Lmao')),
-        body: Center(
-          child: SelectionArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                Text('Selectable text'),
-                SelectionContainer.disabled(child: Text('Non-selectable text')),
-                Text('Selectable text'),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    throw UnimplementedError();
   }
-
 
 }
