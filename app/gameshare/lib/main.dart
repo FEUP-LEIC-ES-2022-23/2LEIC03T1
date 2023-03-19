bef877ca7680d90ef435c1f5ed1573a5e6092b77
dsantosferreira <unidsferreira2003@gmail.com>
2023-03-19 19:34:33 +0000

    Adds fetching games from RAWG api feature

--- app/gameshare/lib/main.dart
+++ app/gameshare/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:gameshare/services/api_requests.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  fetchGames(page: 2);
   runApp(const MyApp());
 }
 
@@ -10,7 +14,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      title: 'haahah',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Lmao')),
+        body: Center(
+          child: SelectionArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Text('Selectable text'),
+                SelectionContainer.disabled(child: Text('Non-selectable text')),
+                Text('Selectable text'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
 
