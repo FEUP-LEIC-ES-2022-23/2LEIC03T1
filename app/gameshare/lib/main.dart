d4828fcb501c5472fe9d59c804c74d3b71d356da
marco-vb <marcofilipe03@hotmail.com>
2023-03-24 19:19:49 +0000

    Started login page

--- app/gameshare/lib/main.dart
+++ app/gameshare/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:gameshare/view/screens/login.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,8 +15,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      title: 'GameShare',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
   }
-
 }
---
+++ app/gameshare/lib/view/screens/login.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:gameshare/services/auth.dart';
+import 'package:gameshare/view/screens/home.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String? errorMessage;
+
+  final TextEditingController userController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> signIn() async {
+    try {
+      await Auth().signInEmailPassword(
+        userController.text,
+        passwordController.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() => errorMessage = e.message);
+    }
+  }
+
+  Future<void> signUp() async {
+    try {
+      await Auth().signUpsignInEmailPassword(
+        userController.text,
+        passwordController.text,
+      );
+      errorMessage = null;
+    } on FirebaseAuthException catch (e) {
+      setState(() => errorMessage = e.message);
+    }
+  }
+
+  Widget _title(String title) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontFamily: 'MontserratAlternates',
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _entryField(String title, {bool isPassword = false}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _title(title),
+          TextField(
+            style: const TextStyle(color: Colors.black),
+            obscureText: isPassword,
+            controller: isPassword ? passwordController : userController,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              fillColor: Color(0xfff3f3f4),
+              filled: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _emailPasswordWidget() {
+    return Column(
+      children: <Widget>[
+        _entryField('Email/Username'),
+        _entryField('Password', isPassword: true),
+      ],
+    );
+  }
+
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () async {
+        errorMessage = null;
+        await signIn();
+        if (errorMessage == null && context.mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const HomePage(),
+            ),
+          );
+        }
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Color(0xffd9d9d9),
+              offset: Offset(2, 4),
+              blurRadius: 5,
+              spreadRadius: 2,
+            ),
+          ],
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: <Color>[
+              Color(0xff00bfa5),
+              Color(0xff00acc1),
+            ],
+          ),
+        ),
+        child: const Text(
+          'Login',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _errorMessage() {
+    if (errorMessage != null) {
+      return Text(
+        errorMessage!,
+        style: const TextStyle(
+          color: Colors.red,
+          fontSize: 13,
+        ),
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(
+                height: 150,
+              ),
+              _title('Login Page'),
+              const SizedBox(
+                height: 80,
+              ),
+              _emailPasswordWidget(),
+              const SizedBox(
+                height: 20,
+              ),
+              _errorMessage(),
+              const SizedBox(
+                height: 20,
+              ),
+              _submitButton(),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
