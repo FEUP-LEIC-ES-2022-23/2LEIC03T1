7844bdc8768871813c8ccb6b9e6c9b9dee8bf774
dsantosferreira <unidsferreira2003@gmail.com>
2023-04-12 17:47:11 +0100

    Added genre creation from json object test

---
+++ app/gameshare/test/model/genre_test.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:gameshare/model/genre.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Crating a genre object', () {
+    const List<dynamic> data = [{'name': 'Action', 'slug': 'action'}, {'name': 'Puzzle', 'slug': 'puzzle'}];
+
+    Genre genreAction = Genre.fromJson(data, 0);
+    Genre genrePuzzle = Genre.fromJson(data, 1);
+
+    expect(genreAction.name == 'Action', true);
+    expect(genreAction.slug == 'action', true);
+    expect(genrePuzzle.name == 'Puzzle', true);
+    expect(genrePuzzle.slug == 'puzzle', true);
+  });
+}
