d81cca3a0d9bfa9e052658af33ca00d1d6213ce9
Jose Quintas <jose.nuno.quintas@gmail.com>
2023-04-16 20:35:42 +0100

    Added ColorScheme to light and dark ThemeData. Deprecated code hasn't been deleted yet

--- app/gameshare/lib/consts/theme_data.dart
+++ app/gameshare/lib/consts/theme_data.dart
@@ -4,6 +4,7 @@ import 'package:gameshare/consts/app_colors.dart';
 
 
 ThemeData lightTheme=ThemeData(
+  //Remover?
   brightness: Brightness.light,
   scaffoldBackgroundColor: Colors.white,
   primaryColor: MyAppColors.midnightBlue,
@@ -16,19 +17,20 @@ ThemeData lightTheme=ThemeData(
       color: MyAppColors.lightGreen,
       fontSize: 25,
     ),
-
   ),
 
   textButtonTheme: const TextButtonThemeData(
     style: ButtonStyle(
       iconColor: MaterialStatePropertyAll<Color>(MyAppColors.lightBlue),
       iconSize: MaterialStatePropertyAll<double>(30),
-  )
+    )
   ),
+
   bottomNavigationBarTheme: const BottomNavigationBarThemeData(
     selectedItemColor:  MyAppColors.lightGreen,
     unselectedItemColor:  MyAppColors.lightBlue,
   ),
+
     progressIndicatorTheme: const ProgressIndicatorThemeData(
       color: MyAppColors.lightBlue,
     ),
@@ -36,17 +38,41 @@ ThemeData lightTheme=ThemeData(
   inputDecorationTheme: const InputDecorationTheme(
     fillColor: MyAppColors.lightGrey,
   ),
-  accentColor: MyAppColors.purple,
+  accentColor: MyAppColors.purple, //Remover
+  colorScheme: ColorScheme(
+      brightness: Brightness.light,
 
+      primary: MyAppColors.darkBlue,
+      onPrimary: MyAppColors.lightGreen,
+
+      secondary: MyAppColors.purple,
+      onSecondary: Colors.white,
+
+      error: Colors.red, //mudar possivelmente
+      onError: Colors.white, //mudar tbm
+
+      background: Colors.white,
+      onBackground: Colors.black,
+
+      surface: Colors.white, //mudar (possivelmente)
+      onSurface: Colors.black, //mudar tbm
+
+      //Não Obrigatorios (podem ser removidos)
+      shadow: MyAppColors.grey,
+      primaryContainer: Colors.white,
+      onPrimaryContainer: Colors.black,
+  )
 
 
 );
 
 ThemeData darkTheme=ThemeData(
+  //Remover?
   brightness: Brightness.dark,
   scaffoldBackgroundColor: MyAppColors.midnightBlue ,
   primaryColor: Colors.white ,
   backgroundColor: MyAppColors.darkBlue,
+
   appBarTheme: const AppBarTheme(
     elevation: 0.0,
     backgroundColor: MyAppColors.darkBlue,
@@ -74,6 +100,32 @@ ThemeData darkTheme=ThemeData(
     inputDecorationTheme: InputDecorationTheme(
       fillColor: MyAppColors.darkBlue,
     ),
-  accentColor: MyAppColors.lightGreen,
+  accentColor: MyAppColors.lightGreen, //Remover
+  colorScheme: ColorScheme(
+      brightness: Brightness.dark,
 
+      primary: MyAppColors.darkBlue,
+      onPrimary: MyAppColors.lightGreen,
+
+      secondary: MyAppColors.purple,
+      onSecondary: Colors.white,
+
+      error: Colors.red, //mudar possivelmente
+      onError: Colors.white, //mudar possivelmente
+
+      background: MyAppColors.midnightBlue,
+      onBackground: Colors.white,
+
+      surface: MyAppColors.midnightBlue, //mudar (possivelmente)
+      onSurface: Colors.white, //mudar tbm
+
+
+      //Não Obrigatórios (podem ser removidos)
+      shadow: MyAppColors.grey,
+      primaryContainer: MyAppColors.midnightBlue,
+      onPrimaryContainer: Colors.white,
+
+      secondaryContainer: MyAppColors.lightGreen,
+      onSecondaryContainer: Colors.white
+  )
 );
