5e49d889ae3dde86bf56de4916519b0abed8cd36
marco-vb <marcofilipe03@hotmail.com>
2023-04-17 01:52:48 +0100

    Tests for login page done, mocked dependencies

--- app/gameshare/test/view/screens/login_test.dart
+++ app/gameshare/test/view/screens/login_test.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:mockito/mockito.dart';
+import 'package:gameshare/services/auth.dart';
 import 'package:gameshare/view/components/helper_widgets.dart';
 import 'package:gameshare/view/components/input.dart';
-import 'package:mockito/mockito.dart';
 import 'package:gameshare/view/screens/login.dart';
-import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 
-class TestApp extends StatelessWidget {
-  const TestApp({Key? key}) : super(key: key);
+class MockAuth extends Mock implements Auth {
+  final MockFirebaseAuth auth;
+
+  MockAuth({MockFirebaseAuth? auth}) : auth = auth ?? MockFirebaseAuth();
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginPage());
+  Future<bool> signInEmailPassword(String email, String password) async {
+    if (email == 'good_email' && password == 'good_pwd') return true;
+    return false;
   }
-}
-
-class MockAuthenticator extends Mock {
-  void signInEmailPassword(String? email, String? password);
 }
 
 void main() {
   testWidgets('Login screen has expected text', (WidgetTester tester) async {
-    await tester.pumpWidget(const TestApp());
+    final MockAuth auth = MockAuth(auth: MockFirebaseAuth());
+    await tester.pumpWidget(MaterialApp(home: LoginPage(auth: auth)));
 
     expect(find.text("GAMESHARE"), findsOneWidget);
     expect(find.text('Email/Username'), findsOneWidget);
@@ -34,20 +35,46 @@ void main() {
   });
 
   testWidgets('Login screen has expected buttons', (WidgetTester tester) async {
-    await tester.pumpWidget(const TestApp());
+    final MockAuth auth = MockAuth(auth: MockFirebaseAuth());
+    await tester.pumpWidget(MaterialApp(home: LoginPage(auth: auth)));
 
     expect(find.byType(InkWell), findsWidgets);
     expect(find.byType(MyCheckBox), findsOneWidget);
     expect(find.byType(EntryFieldList), findsOneWidget);
   });
 
-  testWidgets('Screen changes to register screen on button press',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(const TestApp());
+  testWidgets('Screen changes on button press', (WidgetTester tester) async {
+    final MockAuth auth = MockAuth(auth: MockFirebaseAuth());
+    await tester.pumpWidget(MaterialApp(home: LoginPage(auth: auth)));
 
     await tester.tap(find.text('Don\'t have an account? Create one'));
     await tester.pumpAndSettle();
 
     expect(find.text('Register'), findsOneWidget);
+  });
+
+  testWidgets('Authentication should work as expected', (widgetTester) async {
+    final MockAuth auth = MockAuth(auth: MockFirebaseAuth());
+    final MockUser user = MockUser();
+    final Finder emailField = find.byType(EntryField).at(0);
+    final Finder passwordField = find.byType(EntryField).at(1);
+    final Finder loginButton = find.text('Login');
+
+    await widgetTester.pumpWidget(MaterialApp(home: LoginPage(auth: auth)));
+
+    await widgetTester.enterText(emailField, 'good_email');
+    await widgetTester.enterText(passwordField, 'good_pwd');
+    await widgetTester.tap(loginButton);
+    await widgetTester.pumpAndSettle();
+
+    expect(find.text("Invalid email or password"), findsNothing);
+    expect(user, isNotNull);
+
+    await widgetTester.enterText(emailField, "bad_email");
+    await widgetTester.enterText(passwordField, "bad_pwd");
+    await widgetTester.tap(loginButton);
+    await widgetTester.pumpAndSettle();
+
+    expect(find.text("Invalid email or password"), findsOneWidget);
   });
 }
