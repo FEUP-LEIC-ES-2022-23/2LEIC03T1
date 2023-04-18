2e18d603d452b84afb9d62d1acbe8c1b17d30383
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2023-04-18 18:29:53 +0100

    Making the about section of the game page

---
+++ app/gameshare/lib/view/components/text_section.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gameshare/consts/app_colors.dart';
+import 'package:gameshare/view/components/section_title.dart';
+import "dart:convert";
+
+
+
+class TextSection extends StatefulWidget {
+  TextSection({
+    super.key,
+    required this.title,
+    required this.text,
+  });
+  final String title;
+  final String text;
+
+  @override
+  State<TextSection> createState() => _TextSection(title: title,text: text);
+
+}
+class _TextSection extends State<TextSection>{
+
+  _TextSection({
+    required this.title,
+    required this.text,
+  });
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Html(String html){
+
+    String withBreaks = html.replaceAll("<br />", '\n');
+    RegExp exp = RegExp(r"<[^>]*>",multiLine: true,caseSensitive: true);
+    String withoutElements = withBreaks.replaceAll(exp, ' ');
+    String withApostrophe = withoutElements.replaceAll("&#39;", "'");
+    const utf= Utf8Decoder();
+
+    String withoutSpecialChars= utf.convert(withApostrophe.runes.toList());
+    return withoutSpecialChars;
+  }
+
+  final String title;
+  final String text;
+  late bool showMore=false;
+  List<Widget> getText(){
+    String mainText;
+    String buttonText;
+    if(showMore){
+      mainText=text;
+      buttonText="Show less";
+    }
+    else{
+      mainText= text.substring(0,300);
+      buttonText="Show More";
+    }
+
+
+
+    return [
+      Text(
+          Html(mainText),
+          style: const TextStyle(fontSize: 20)
+      ),
+      SizedBox(height: 20,),
+      ElevatedButton(
+          onPressed: (){
+            setState(() {
+              showMore=!showMore;
+            });
+
+          },
+
+          child:Text(buttonText,textAlign: TextAlign.center),
+        style: ButtonStyle(
+          alignment: Alignment.center,
+          // backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).accentColor),
+          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.only(left: 40,right: 40,top:5,bottom: 5)),
+          // animationDuration: Duration(seconds: 10),
+
+
+        ),
+
+
+      )
+    ];
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Column( children: [
+        SectionTitle(title: title),
+        SizedBox(height: 15,),
+        ...getText(),
+
+
+      ],
+      ),
+    );
+  }
+
+
+}
