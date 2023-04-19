ad4d15e4eae7493522ff663c1f9e7d659963e7fa
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2023-04-19 15:32:46 +0100

    Bug Fix: Directories of the test_driver where wrong

--- app/gameshare/test_driver/app_test.dart
+++ app/gameshare/test_driver/app_test.dart
@@ -1,25 +1,30 @@
 import 'dart:async';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
+import 'package:glob/glob.dart';
 
+import 'steps/dark_light_mode/given.dart';
 import 'steps/dark_light_mode/tap_on_button.dart';
 import 'steps/dark_light_mode/theme_change.dart';
 
-import 'package:glob/glob.dart';
+
 
 Future<void> main() {
   final config = FlutterTestConfiguration()
-    ..features = [Glob(r'test_driver/features/**.feature')]
+    ..features = [Glob(r'features/**.feature')]
     ..reporters = [
       ProgressReporter(),
       TestRunSummaryReporter(),
-      JsonReporter(path: './report.json')
-    ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
-    ..hooks = []
-    ..stepDefinitions = [TapButtonNTimesStep(), ThemeChange()]
+      JsonReporter(path: './report.json'),
+    ]
+    ..stepDefinitions = [
+      GivenOpenDrawer(),
+      TapButtonNTimesStep(),
+      ThemeChange(),
+      EnterApp()
+    ]
     ..customStepParameterDefinitions = []
     ..restartAppBetweenScenarios = true
-    ..targetAppPath = "test_driver/app.dart";
-  // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
+    ..targetAppPath = 'test_driver/app.dart';
   return GherkinRunner().execute(config);
 }
---
+++ app/gameshare/test_driver/steps/dark_light_mode/given.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:gherkin/gherkin.dart';
+
+StepDefinitionGeneric EnterApp() {
+  return given<FlutterWorld>(
+    'I enter the app',
+        ( context) async {
+        return;
+      });
+}
