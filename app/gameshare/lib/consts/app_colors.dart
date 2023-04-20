3e26a82f9f8548ebf9345088574edc21547a3493
ze <joseapmartins2003@gmail.com>
2023-04-20 03:45:44 +0100

    Made some new tests. Also added _scrollToTap method on topBar Gameshare Text

--- app/gameshare/lib/consts/app_colors.dart
+++ app/gameshare/lib/consts/app_colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class MyAppColors {
-  static const  Color lightGreen = Color(0xff08C076);
+  static const Color lightGreen = Color(0xff08C076);
   static const Color darkBlue = Color(0xFF1B274B);
   static const Color midnightBlue = Color(0xFF1F2D5A);
   static const Color lightBlue =Color(0xFF00b3FF);
