b215f404ccd69915de085bf3d5ba045a0395507c
dsantosferreira <unidsferreira2003@gmail.com>
2023-05-04 07:22:09 +0100

    Aligned delete button to the right of the row

--- app/gameshare/lib/view/components/review_card.dart
+++ app/gameshare/lib/view/components/review_card.dart
@@ -140,30 +140,34 @@ class ReviewUser extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Container(child: image),
-        Flexible(
+        Expanded(
           flex: 1,
-          child: InkWell(
-              onTap: () {
-                bool cond = false;
-                if (Auth().user != null) {
-                  cond = (Auth().user!.email) == name;
-                }
-                Navigator.pushReplacement(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (_, __, ____) =>
-                        UserPage(user: name, isUser: cond),
-                    transitionDuration: const Duration(seconds: 0),
-                  ),
-                );
-              },
-              child: Text(
-                name,
-                overflow: TextOverflow.ellipsis,
-                style: GoogleFonts.montserratAlternates(
-                    fontWeight: FontWeight.bold, fontSize: 15),
-              )),
+          child: Row(
+            children: [
+              Container(child: image),
+              InkWell(
+                onTap: () {
+                  bool cond = false;
+                  if (Auth().user != null) {
+                    cond = (Auth().user!.email) == name;
+                  }
+                  Navigator.pushReplacement(
+                    context,
+                    PageRouteBuilder(
+                      pageBuilder: (_, __, ____) =>
+                          UserPage(user: name, isUser: cond),
+                      transitionDuration: const Duration(seconds: 0),
+                    ),
+                  );
+                },
+                child: Text(
+                  name,
+                  overflow: TextOverflow.ellipsis,
+                  style: GoogleFonts.montserratAlternates(
+                      fontWeight: FontWeight.bold, fontSize: 15),
+                )),
+            ],
+          ),
         ),
         if (currUser != null && currUser!.email == name)
           Expanded(
